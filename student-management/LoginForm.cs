22c74205d0acb8bdab82055db4d5ed978dc91273
mimhle <40392166+mimhle@users.noreply.github.com>
2023-12-08 16:36:07 +0700

    Refactor

--- student-management/LoginForm.cs
+++ student-management/LoginForm.cs
@@ -4,11 +4,11 @@ using System.Windows.Forms;
 namespace studentManagement {
     public partial class LoginForm : Form {
         private readonly Database _db = Program.db;
+
         public LoginForm() {
             InitializeComponent();
 
             // test code for Database.cs
-            //var db = new Database(Program.dbLocation);
             _db.insertStudent("SV01", "Nguyen Van A", "1999-01-01", "Nam", "CNTT", "48.CNTT.A");
             _db.insertStudent("SV02", "Nguyen Van A", "1999-01-01", "Nam", "CNTT", "48.CNTT.A");
             _db.insertStudent("SV03", "Nguyen Van B", "1999", "Nam", "CNTT", "48.CNTT.A", true);
@@ -29,13 +29,14 @@ namespace studentManagement {
             _db.insertSubjectClassStudent("SV03", "XSTK001");
             _db.insertScore("SV02", "LTWIN", 10);
             _db.insertScore("SV03", "XSTK", 9);
-            
+
             //db.removeStudent("SV01");
             //db.removeStudent("SV04");
             Console.WriteLine(_db.updatePassword("SV02", "123456"));
             Console.WriteLine(_db.checkLogin("SV02", "SV02"));
             Console.WriteLine(_db.checkLogin("SV02", "123456"));
         }
+
         //all form display
         //if it iNum == 1 return show or else hide
         private void loginForm(int iNum) {
@@ -43,20 +44,19 @@ namespace studentManagement {
                 gbLogin.Show();
                 txtPassWord.Clear();
                 txtUserName.Clear();
-            }
-            else gbLogin.Hide();
+            } else gbLogin.Hide();
         }
 
         private void menuForm(int iNum) {
-            if(iNum == 1) {
+            if (iNum == 1) {
                 gbMenu.Show();
-                btnLogout.Show() ;
-            }
-            else {
+                btnLogout.Show();
+            } else {
                 gbMenu.Hide();
                 btnLogout.Hide();
             }
         }
+
         private void btnExit_Click(object sender, EventArgs e) {
             Close();
         }
@@ -67,13 +67,10 @@ namespace studentManagement {
         }
 
         private void btnLogin_Click(object sender, EventArgs e) {
-            // menuForm(1);
-            // loginForm(0);
             if (_db.checkLogin(txtUserName.Text, txtPassWord.Text)) {
                 menuForm(1);
                 loginForm(0);
-            }
-            else {
+            } else {
                 MessageBox.Show(@"Sai tên đăng nhập hoặc mật khẩu", @"Lỗi đăng nhập", MessageBoxButtons.OK,
                     MessageBoxIcon.Error);
             }
