82ece68adeb68fc942e0b61b317731fb4b39b22a
mimhle <40392166+mimhle@users.noreply.github.com>
2023-12-08 09:07:47 +0700

    Reformat

--- student-management/LoginForm.cs
+++ student-management/LoginForm.cs
@@ -3,38 +3,38 @@ using System.Windows.Forms;
 
 namespace studentManagement {
     public partial class LoginForm : Form {
-        private readonly Database db = Program.db;
+        private readonly Database _db = Program.db;
         public LoginForm() {
             InitializeComponent();
 
             // test code for Database.cs
             //var db = new Database(Program.dbLocation);
-            db.insertStudent("SV01", "Nguyen Van A", "1999-01-01", "Nam", "CNTT", "48.CNTT.A");
-            db.insertStudent("SV02", "Nguyen Van A", "1999-01-01", "Nam", "CNTT", "48.CNTT.A");
-            db.insertStudent("SV03", "Nguyen Van B", "1999", "Nam", "CNTT", "48.CNTT.A", true);
-            db.insertStudent("SV04", "Nguyen Van C", "1999", "Nam", "CNTT", "48.CNTT.B", true);
-            db.updatePassword("SV03", "12345");
+            _db.insertStudent("SV01", "Nguyen Van A", "1999-01-01", "Nam", "CNTT", "48.CNTT.A");
+            _db.insertStudent("SV02", "Nguyen Van A", "1999-01-01", "Nam", "CNTT", "48.CNTT.A");
+            _db.insertStudent("SV03", "Nguyen Van B", "1999", "Nam", "CNTT", "48.CNTT.A", true);
+            _db.insertStudent("SV04", "Nguyen Van C", "1999", "Nam", "CNTT", "48.CNTT.B", true);
+            _db.updatePassword("SV03", "12345");
 
-            db.insertFaculty("CNTT", "Cong nghe thong tin");
-            db.insertFaculty("QTKD", "Quan tri kinh doanh");
-            db.insertClass("48.CNTT.A", "Cong nghe thong tin A", "CNTT");
-            db.insertClass("48.CNTT.B", "Cong nghe thong tin B", "CNTT");
+            _db.insertFaculty("CNTT", "Cong nghe thong tin");
+            _db.insertFaculty("QTKD", "Quan tri kinh doanh");
+            _db.insertClass("48.CNTT.A", "Cong nghe thong tin A", "CNTT");
+            _db.insertClass("48.CNTT.B", "Cong nghe thong tin B", "CNTT");
 
-            db.insertSubjectClass("LTWIN001", "HCMUE_LTWin_HK1.2023-2024_901", "CNTT", "LTWIN");
-            db.insertSubjectClass("XSTK001", "HCMUE_XSTK_HK1.2023-2024_701", "CNTT", "XSTK");
-            db.insertSubject("LTWIN", "Lap trinh Windows", "CNTT", 2);
-            db.insertSubject("XSTK", "Xac suat thong ke", "CNTT", 3);
-            db.insertSubjectClassStudent("SV01", "LTWIN001");
-            db.insertSubjectClassStudent("SV02", "LTWIN001");
-            db.insertSubjectClassStudent("SV03", "XSTK001");
-            db.insertScore("SV02", "LTWIN", 10);
-            db.insertScore("SV03", "XSTK", 9);
+            _db.insertSubjectClass("LTWIN001", "HCMUE_LTWin_HK1.2023-2024_901", "CNTT", "LTWIN");
+            _db.insertSubjectClass("XSTK001", "HCMUE_XSTK_HK1.2023-2024_701", "CNTT", "XSTK");
+            _db.insertSubject("LTWIN", "Lap trinh Windows", "CNTT", 2);
+            _db.insertSubject("XSTK", "Xac suat thong ke", "CNTT", 3);
+            _db.insertSubjectClassStudent("SV01", "LTWIN001");
+            _db.insertSubjectClassStudent("SV02", "LTWIN001");
+            _db.insertSubjectClassStudent("SV03", "XSTK001");
+            _db.insertScore("SV02", "LTWIN", 10);
+            _db.insertScore("SV03", "XSTK", 9);
             
             //db.removeStudent("SV01");
             //db.removeStudent("SV04");
-            Console.WriteLine(db.updatePassword("SV02", "123456"));
-            Console.WriteLine(db.checkLogin("SV02", "SV02"));
-            Console.WriteLine(db.checkLogin("SV02", "123456"));
+            Console.WriteLine(_db.updatePassword("SV02", "123456"));
+            Console.WriteLine(_db.checkLogin("SV02", "SV02"));
+            Console.WriteLine(_db.checkLogin("SV02", "123456"));
         }
         //all form display
         //if it iNum == 1 return show or else hide
@@ -69,7 +69,7 @@ namespace studentManagement {
         private void btnLogin_Click(object sender, EventArgs e) {
             // menuForm(1);
             // loginForm(0);
-            if (db.checkLogin(txtUserName.Text, txtPassWord.Text)) {
+            if (_db.checkLogin(txtUserName.Text, txtPassWord.Text)) {
                 menuForm(1);
                 loginForm(0);
             }
